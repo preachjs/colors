d91911523819624ad3b5b49e54d6b0e58f3b159c
reaper <ahoy@barelyhuman.dev>
2025-03-29 16:47:09 +0530

    fix: allow card clicks to copy

--- docs/main.js
+++ docs/main.js
@@ -1,9 +1,6 @@
-import { colors } from "./preach.js";
-import { kebabCase } from "https://esm.sh/change-case@5.4.4";
 import Toastify from "https://esm.sh/toastify-js@1.12.0";
 
 const copyCSSBtn = document.getElementById("copy-css");
-const palette = document.getElementById("palette");
 
 const cssStyles = await fetch("/preach.css").then((d) => d.text());
 
@@ -11,31 +8,18 @@ copyCSSBtn.addEventListener("click", () => {
   copy(cssStyles);
 });
 
-for (let colorKey in colors) {
-  const color = colors[colorKey];
-  const div = document.createElement("div");
-  div.classList.add("card");
-  div.dataset.color = `${color}/${colorKey}`;
-  div.style.background = `var(--preach--${kebabCase(colorKey)})`;
-  if (colorKey === "text") {
-    div.style.color = `var(--preach--surface)`;
-  } else if (colorKey.endsWith("Light")) {
-    div.style.color = `var(--preach--${kebabCase(colorKey.replace(/Light$/, ""))})`;
-  } else if (colors[colorKey + "Light"]) {
-    div.style.color = `var(--preach--${kebabCase(colorKey + "Light")})`;
-  } else {
-    div.style.color = "var(--preach-text)";
+const colorCards = document.querySelectorAll(".color-box");
+
+for (let box of colorCards) {
+  const variable = box.dataset.color;
+  const color = window
+    .getComputedStyle(document.body)
+    .getPropertyValue(variable);
+  if (!color) {
+    console.log({ missingVariable: variable });
   }
 
-  palette.appendChild(div);
-}
-
-const colorCards = palette.querySelectorAll(".card");
-for (let index = 0; index < colorCards.length; index++) {
-  const element = colorCards[index];
-
-  element.addEventListener("click", () => {
-    const color = element.getAttribute("data-color").split("/")[0];
+  box.addEventListener("click", () => {
     copy(color);
   });
 }
